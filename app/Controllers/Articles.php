4b1816921f464350f4c8974d8850085929a2ceec
Robby de Leon <robbydeleon@gmail.com>
2024-05-13 15:07:20 +0800

    3.1 - Display Dynamic Data in a View

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -6,6 +6,13 @@ class Articles extends BaseController
 {
   public function index()
   {
-    return view('Articles/index');
+    $data = [
+      ['title' => 'First Article', 'content' => 'Content of the first article.'],
+      ['title' => 'Second Article', 'content' => 'Content of the second article.']
+    ];
+    
+    return view('Articles/index', [
+      'articles' => $data
+    ]);
   }
 }
--- app/Views/Articles/index.php
+++ app/Views/Articles/index.php
@@ -4,4 +4,11 @@
 
 <?= $this->section('content') ?>
   <h1>Articles Page</h1>
+
+  <?php foreach ($articles as $article): ?>
+    <article>
+      <h2><?= $article['title'] ?></h2>
+      <p><?= $article['content'] ?></p>
+    </article>
+  <?php endforeach; ?>
 <?= $this->endSection() ?>
