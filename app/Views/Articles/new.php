c38a9715591495c25bd0c5fe649e1852f39eac68
Robby de Leon <robbydeleon@gmail.com>
2024-05-13 16:43:33 +0800

    4.4 Add form and input elements

--- app/Views/Articles/new.php
+++ app/Views/Articles/new.php
@@ -4,4 +4,14 @@
 
 <?= $this->section('content') ?>
   <h1>New Article</h1>
+
+  <?= form_open('articles/create') ?>
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title">
+
+    <label for="content">Content</label>
+    <textarea name="content" id="content"></textarea>
+
+    <button>Save</button>
+  <?= form_close() ?>
 <?= $this->endSection() ?>
